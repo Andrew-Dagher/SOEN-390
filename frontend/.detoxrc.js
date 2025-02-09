96cb8278b96a154794b2cf82b1cc6bf7736b7df4
Andrew Harissi Dagher <andrew.h.dagher@gmail.com>
2025-02-08 23:31:39 -0500

    changed detox configs

--- frontend/.detoxrc.js
+++ frontend/.detoxrc.js
@@ -12,13 +12,13 @@ module.exports = {
   apps: {
     'ios.debug': {
       type: 'ios.app',
-      binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/YOUR_APP.app',
-      build: 'xcodebuild -workspace ios/YOUR_APP.xcworkspace -scheme YOUR_APP -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build'
+      binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/SOEN-390.app',
+      build: 'xcodebuild -workspace ios/SOEN-390.xcworkspace -scheme SOEN-390 -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build'
     },
     'ios.release': {
       type: 'ios.app',
-      binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/YOUR_APP.app',
-      build: 'xcodebuild -workspace ios/YOUR_APP.xcworkspace -scheme YOUR_APP -configuration Release -sdk iphonesimulator -derivedDataPath ios/build'
+      binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/SOEN-390.app',
+      build: 'xcodebuild -workspace ios/SOEN-390.xcworkspace -scheme SOEN-390 -configuration Release -sdk iphonesimulator -derivedDataPath ios/build'
     },
     'android.debug': {
       type: 'android.apk',
