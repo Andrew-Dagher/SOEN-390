9547af6452cce18f95d2ba571376f73ff7d3118c
SoumyBhow <soumybhow1.0@gmail.com>
2025-02-12 10:08:01 -0500

    Add stack

--- frontend/__tests__/BottomNavBar-test.jsx
+++ frontend/__tests__/BottomNavBar-test.jsx
@@ -1,14 +1,20 @@
 import { render } from "@testing-library/react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import BottomNavBar from "../app/components/BottomNavBar/BottomNavBar";
 import { AppSettingsProvider } from "../app/TextSizeContext";
-import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createNativeStackNavigator();
+
 describe("<BottomNavBar />", () => {
   test("Bottom Nav Bar renders correctly", () => {
     const { getByTestId } = render(
       <AppSettingsProvider>
         <NavigationContainer>
-          <BottomNavBar />
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={BottomNavBar} />
+          </Stack.Navigator>
         </NavigationContainer>
       </AppSettingsProvider>
     );
