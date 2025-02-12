adfa73c044ed02bbc0b950ea94a3dd46719d49b0
SoumyBhow <soumybhow1.0@gmail.com>
2025-02-12 09:57:02 -0500

    Add app settings

--- frontend/__tests__/HomeHeader-test.jsx
+++ frontend/__tests__/HomeHeader-test.jsx
@@ -1,10 +1,14 @@
 import { render, within } from "@testing-library/react-native";
 
 import HomeHeader from "../app/components/Homescreen/HomeHeader/HomeHeader";
-
+import { AppSettingsProvider } from "../app/TextSizeContext";
 describe("<HomeHeader />", () => {
   test("Text renders correctly on HomeScreen", () => {
-    const { getByTestId } = render(<HomeHeader name="John Doe" />);
+    const { getByTestId } = render(
+      <AppSettingsProvider>
+        <HomeHeader name="John Doe" />
+      </AppSettingsProvider>
+    );
 
     const viewComponent = getByTestId("home-header");
     const getByText = within(viewComponent);
