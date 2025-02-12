3f620e7e5b4b83dac77a993e61b4e95f784b7d46
SoumyBhow <soumybhow1.0@gmail.com>
2025-02-12 09:42:50 -0500

    Fix imports

--- frontend/__tests__/HomeScreen-test.jsx
+++ frontend/__tests__/HomeScreen-test.jsx
@@ -3,9 +3,8 @@ import { render } from "@testing-library/react-native";
 import HomeScreen from "../app/screens/home/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
-import { AppSettingsProvider } from "./TextSizeContext";
-import { TextSizeProvider } from "./TextSizeContext";
-
+import { AppSettingsProvider } from "../app/TextSizeContext";
+import { TextSizeProvider } from "../app/TextSizeContext";
 describe("<HomeScreen />", () => {
   test("Text renders correctly on HomeScreen", () => {
     const { getByTestId } = render(
