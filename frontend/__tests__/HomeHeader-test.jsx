e375182d25e027afddd702750b0daa2b84254a3d
SoumyBhow <soumybhow1.0@gmail.com>
2025-02-12 09:59:56 -0500

    Try getByText again

--- frontend/__tests__/HomeHeader-test.jsx
+++ frontend/__tests__/HomeHeader-test.jsx
@@ -4,16 +4,14 @@ import HomeHeader from "../app/components/Homescreen/HomeHeader/HomeHeader";
 import { AppSettingsProvider } from "../app/TextSizeContext";
 describe("<HomeHeader />", () => {
   test("Text renders correctly on HomeScreen", () => {
-    const { getByTestId } = render(
+    const { getByText, getByTestId } = render(
       <AppSettingsProvider>
         <HomeHeader name="John Doe" />
       </AppSettingsProvider>
     );
 
     const viewComponent = getByTestId("home-header");
-    const getByText = within(viewComponent);
 
-    expect(viewComponent).toBeTruthy();
     expect(getByText("Welcome Back")).toBeTruthy();
     expect(getByText("John Doe")).toBeTruthy();
   });
