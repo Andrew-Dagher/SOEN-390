8f3b977520fd5b9b7dbe6734138abe3d65094bea
SoumyBhow <soumybhow1.0@gmail.com>
2025-02-12 10:05:24 -0500

    Add Nav Container

--- frontend/__tests__/BottomNavBar-test.jsx
+++ frontend/__tests__/BottomNavBar-test.jsx
@@ -2,11 +2,14 @@ import { render } from "@testing-library/react-native";
 
 import BottomNavBar from "../app/components/BottomNavBar/BottomNavBar";
 import { AppSettingsProvider } from "../app/TextSizeContext";
+import { NavigationContainer } from "@react-navigation/native";
 describe("<BottomNavBar />", () => {
   test("Bottom Nav Bar renders correctly", () => {
     const { getByTestId } = render(
       <AppSettingsProvider>
-        <BottomNavBar />
+        <NavigationContainer>
+          <BottomNavBar />
+        </NavigationContainer>
       </AppSettingsProvider>
     );
 
