dfed9acd47e48aef260592501eead51aefc161d5
SoumyBhow <soumybhow1.0@gmail.com>
2025-02-12 09:45:10 -0500

    Remove clerk

--- frontend/__tests__/HomeScreen-test.jsx
+++ frontend/__tests__/HomeScreen-test.jsx
@@ -2,25 +2,18 @@ import { render } from "@testing-library/react-native";
 
 import HomeScreen from "../app/screens/home/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
-import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
 import { AppSettingsProvider } from "../app/TextSizeContext";
 import { TextSizeProvider } from "../app/TextSizeContext";
 describe("<HomeScreen />", () => {
   test("Text renders correctly on HomeScreen", () => {
     const { getByTestId } = render(
-      <ClerkProvider
-        publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
-      >
-        <ClerkLoaded>
-          <AppSettingsProvider>
-            <TextSizeProvider>
-              <NavigationContainer>
-                <HomeScreen />
-              </NavigationContainer>
-            </TextSizeProvider>
-          </AppSettingsProvider>
-        </ClerkLoaded>
-      </ClerkProvider>
+      <AppSettingsProvider>
+        <TextSizeProvider>
+          <NavigationContainer>
+            <HomeScreen />
+          </NavigationContainer>
+        </TextSizeProvider>
+      </AppSettingsProvider>
     );
 
     const viewComponent = getByTestId("home-screen");
