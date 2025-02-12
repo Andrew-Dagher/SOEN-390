272d96df24854f67c14a9ad34f76960afa0389cd
SoumyBhow <soumybhow1.0@gmail.com>
2025-02-12 12:22:11 -0500

    Fix thing hopefully

--- frontend/__tests__/SettingsScreen-test.jsx
+++ frontend/__tests__/SettingsScreen-test.jsx
@@ -1,11 +1,24 @@
-import { render } from '@testing-library/react-native';
-import SettingsScreen from '../app/screens/settings/settingsScreen';
+import { render } from "@testing-library/react-native";
+import SettingsScreen from "../app/screens/settings/settingsScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AppSettingsProvider } from "../app/TextSizeContext";
+const Stack = createNativeStackNavigator();
+describe("<Setting />", () => {
+  test("Text renders correctly on Settings Screen", () => {
+    const { getByTestId } = render(
+      <AppSettingsProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Settings" component={BottomNavBar} />
+            <SettingsScreen />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AppSettingsProvider>
+    );
 
-describe('<Setting />', () => {
-    test('Text renders correctly on Settings Screen', () => {
-    const { getByTestId } = render(<SettingsScreen />);
+    const viewComponent = getByTestId("settings-screen");
 
-    const viewComponent = getByTestId("settings-screen")
-
-    expect(viewComponent).toBeTruthy(); });
+    expect(viewComponent).toBeTruthy();
+  });
 });
