41d8a87e081942d68df48a1a4bad3805cc2aec09
SoumyBhow <soumybhow1.0@gmail.com>
2025-02-12 12:24:05 -0500

    Try to fix

--- frontend/__tests__/LoginScreen-test.jsx
+++ frontend/__tests__/LoginScreen-test.jsx
@@ -1,11 +1,24 @@
-import { render } from '@testing-library/react-native';
-import LoginScreen from '../app/screens/login/LoginScreen';
+import { render } from "@testing-library/react-native";
+import LoginScreen from "../app/screens/login/LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AppSettingsProvider } from "../app/TextSizeContext";
+const Stack = createNativeStackNavigator();
+describe("<Login />", () => {
+  test("Text renders correctly on Login Screen", () => {
+    const { getByTestId } = render(
+      <AppSettingsProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Settings" component={BottomNavBar} />
+            <LoginScreen />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AppSettingsProvider>
+    );
 
-describe('<Login />', () => {
-    test('Text renders correctly on Login Screen', () => {
-    const { getByTestId } = render(<LoginScreen />);
+    const viewComponent = getByTestId("login-screen");
 
-    const viewComponent = getByTestId("login-screen")
-
-    expect(viewComponent).toBeTruthy(); });
+    expect(viewComponent).toBeTruthy();
+  });
 });
