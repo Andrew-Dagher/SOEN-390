c26393413b0e8570b06cd8dbacfd6db09f64f3f1
SoumyBhow <soumybhow1.0@gmail.com>
2025-02-12 09:36:13 -0500

    Add nav container

--- frontend/__tests__/HomeScreen-test.jsx
+++ frontend/__tests__/HomeScreen-test.jsx
@@ -1,9 +1,14 @@
 import { render } from "@testing-library/react-native";
 
 import HomeScreen from "../app/screens/home/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
 describe("<HomeScreen />", () => {
   test("Text renders correctly on HomeScreen", () => {
-    const { getByTestId } = render(<HomeScreen />);
+    const { getByTestId } = render(
+      <NavigationContainer>
+        <HomeScreen />
+      </NavigationContainer>
+    );
 
     const viewComponent = getByTestId("home-screen");
 
