9d649f410699ec65a7d63c629e0f4b9bc723fc8a
Richard Badir <ricobadir@hotmail.com>
2025-03-16 18:48:10 -0400

    Display stuff and log coordinates

--- frontend/app/screens/Info/BuildingInfoScreen.jsx
+++ frontend/app/screens/Info/BuildingInfoScreen.jsx
@@ -16,6 +16,7 @@ import {
   StatusBar,
   Image,
   Modal,
+  Alert,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import MapPinIcon from "../../components/navigation/Icons/MapPinIcon";
@@ -39,6 +40,102 @@ const floorplanImages = {
   "VL 2": require("../../../assets/floor_plans/VL-2.png"),
 };
 
+
+const hallFloorGraphs= {
+  // HALL BUILDING NODES
+  1: {
+    Hall_Entrance: { x: 110, y: 360, neighbors: ['Main_walway', 'Hall_1st_Basement_Escalator'] },
+    Main_walway: { x: 110, y: 292, neighbors: ['Hall_Entrance', 'Hallway'] },
+    Hallway: { x: 265, y: 292, neighbors: ['Main_walway', 'Hall_1st_Elevator'] },
+    Hall_1st_Elevator: { x: 265, y: 302, neighbors: ['Hallway'] },
+    Hall_1st_Basement_Escalator: { x: 275, y: 360, neighbors: ['Hall_Entrance'] },
+  },
+  2: {},
+  8: {
+    // hallway intersections
+    hallwayLowerLeftCorner: { x: 70, y: 289, neighbors: ['hallwayMiddleLower', 'hallwayMiddleLeft','h849', 'h847', 'h845', 'h843', 'h842', 'h841', 'h857', 'h855', 'h854', 'h853', 'h852', 'h851'] },
+    hallwayLowerRightCorner: { x: 305, y: 289, neighbors: ['hallwayMiddleLower', 'hallwayHigherRightCorner', 'h837', 'h835', 'h833', 'h832', 'h831', 'h829', 'h827', 'h825', 'h823', 'h822', 'h821', 'h820'] },
+    hallwayHigherRightCorner: { x: 305, y: 78, neighbors: ['hallwayMiddleUpper', 'hallwayLowerRightCorner', 'h827', 'h825', 'h823', 'h822', 'h821', 'h820', 'h819', 'h817', 'h815', 'h813', 'h811', 'mensBathroom'] },
+    hallwayHigherLeftCorner: { x: 65, y: 78, neighbors: ['hallwayHigherRightCorner', 'hallwayMiddleLeft', 'h807', 'h805', 'h803', 'h801', 'womensBathroom', 'h867', 'h865', 'h863', 'h861'] },
+    hallwayMiddleLower: { x: 200, y: 289, neighbors: ['hallwayMiddleMiddle', 'hallwayLowerLeftCorner', 'hallwayLowerRightCorner', 'h845', 'h843', 'h842', 'h841', 'h837', 'h835', 'h832'] },
+    hallwayMiddleMiddle: { x: 200, y: 145, neighbors: ['hallwayMiddleUpper', 'hallwayMiddleLower', 'hallwayMiddleLeft', 'h806', 'h862', 'h860'] },
+    hallwayMiddleUpper: { x: 200, y: 78, neighbors: ['hallwayHigherRightCorner', 'hallwayHigherLeftCorner', 'hallwayMiddleMiddle','h806', 'h862', 'h860', 'h813', 'h811', 'h807', 'h806', 'h805', 'h803', 'h801', 'womensBathroom', 'mensBathroom'] },
+    hallwayMiddleLeft:{ x: 65, y: 145, neighbors: ['hallwayMiddleMiddle', 'hallwayLowerLeftCorner', 'hallwayHigherLeftCorner', 'h863', 'h862', 'h861', 'h860', 'h859', 'h857', 'h855', 'h854', 'h853', 'h852'] },
+
+    // lower section
+    h849: { x: 30, y: 325, neighbors: ['hallwayLowerLeftCorner'] },
+    h847: { x: 70, y: 325, neighbors: ['hallwayLowerLeftCorner'] },
+    h845: { x: 105, y: 325, neighbors: ['hallwayLowerLeftCorner', 'hallwayMiddleLower'] },
+    h843: { x: 135, y: 325, neighbors: ['hallwayLowerLeftCorner', 'hallwayMiddleLower'] },
+    h842: { x: 145, y: 235, neighbors: ['hallwayLowerLeftCorner', 'hallwayMiddleLower'] },
+    h841: { x: 165, y: 325, neighbors: ['hallwayLowerLeftCorner', 'hallwayMiddleLower'] },
+    h837: { x: 235, y: 325, neighbors: ['hallwayLowerRightCorner', 'hallwayMiddleLower'] },
+    h835: { x: 265, y: 325, neighbors: ['hallwayLowerRightCorner', 'hallwayMiddleLower'] },
+    h833: { x: 300, y: 325, neighbors: ['hallwayLowerRightCorner'] },
+    h832: { x: 232, y: 245, neighbors: ['hallwayLowerRightCorner','hallwayMiddleLower'] },
+    h831: { x: 345, y: 325, neighbors: ['hallwayLowerRightCorner'] },
+
+    // Right section
+    h829: { x: 345, y: 285, neighbors: ['hallwayLowerRightCorner'] },
+    h827: { x: 345, y: 220, neighbors: ['hallwayLowerRightCorner', 'hallwayHigherRightCorner'] },
+    h825: { x: 345, y: 190, neighbors: ['hallwayLowerRightCorner', 'hallwayHigherRightCorner'] },
+    h823: { x: 345, y: 155, neighbors: ['hallwayLowerRightCorner', 'hallwayHigherRightCorner'] },
+    h822: { x: 285, y: 222, neighbors: ['hallwayLowerRightCorner', 'hallwayHigherRightCorner'] },
+    h821: { x: 345, y: 120, neighbors: ['hallwayLowerRightCorner', 'hallwayHigherRightCorner'] },
+    h820: { x: 265, y: 168, neighbors: ['hallwayLowerRightCorner', 'hallwayHigherRightCorner'] },
+    h819: { x: 345, y: 85, neighbors: ['hallwayHigherRightCorner'] },
+    h817: { x: 345, y: 50, neighbors: ['hallwayHigherRightCorner'] },
+
+    // Top section
+    h815: { x: 305, y: 40, neighbors: ['hallwayHigherRightCorner'] },
+    h813: { x: 270, y: 40, neighbors: ['hallwayHigherRightCorner', 'hallwayMiddleUpper'] },
+    h811: { x: 230, y: 40, neighbors: ['hallwayHigherRightCorner', 'hallwayMiddleUpper'] },
+    h807: { x: 173, y: 40, neighbors: ['hallwayHigherLeftCorner', 'hallwayMiddleUpper'] },
+    h806: { x: 176, y: 120, neighbors: ['hallwayMiddleMiddle', 'hallwayMiddleUpper'] },
+    h805: { x: 143, y: 40, neighbors: ['hallwayHigherLeftCorner', 'hallwayMiddleUpper'] },
+    h803: { x: 110, y: 40, neighbors: ['hallwayHigherLeftCorner', 'hallwayMiddleUpper'] },
+    h801: { x: 75, y: 40, neighbors: ['hallwayHigherLeftCorner', 'hallwayMiddleUpper'] },
+    womensBathroom: { x: 143, y: 95, neighbors: ['hallwayHigherLeftCorner', 'hallwayMiddleUpper'] },
+    mensBathroom: { x: 235, y: 95, neighbors: ['hallwayHigherRightCorner', 'hallwayMiddleUpper'] },
+
+    // Right section
+    h867: { x: 35, y: 40, neighbors: ['hallwayHigherLeftCorner'] },
+    h865: { x: 27, y: 64, neighbors: ['hallwayHigherLeftCorner'] },
+    h863: { x: 27, y: 90, neighbors: ['hallwayHigherLeftCorner', 'hallwayMiddleLeft'] },
+    h862: { x: 138, y: 165, neighbors: ['hallwayMiddleMiddle', 'hallwayMiddleLeft'] },
+    h861: { x: 27, y: 120, neighbors: ['hallwayHigherLeftCorner', 'hallwayMiddleLeft'] },
+    h860: { x: 98, y: 180, neighbors: ['hallwayMiddleMiddle', 'hallwayMiddleLeft'] },
+    h859: { x: 27, y: 150, neighbors: ['hallwayMiddleLeft'] },
+    h857: { x: 27, y: 180, neighbors: ['hallwayMiddleLeft', 'hallwayLowerLeftCorner'] },
+    h855: { x: 27, y: 215, neighbors: ['hallwayMiddleLeft', 'hallwayLowerLeftCorner'] },
+    h854: { x: 98, y: 215, neighbors: ['hallwayLowerLeftCorner', 'hallwayMiddleLeft'] },
+    h853: { x: 27, y: 248, neighbors: ['hallwayMiddleLeft', 'hallwayLowerLeftCorner'] },
+    h852: { x: 90, y: 235, neighbors: ['hallwayLowerLeftCorner', 'hallwayMiddleLeft'] },
+    h851: { x: 27, y: 285, neighbors: ['hallwayLowerLeftCorner'] },
+  },
+  9: {
+    // Hallway Nodes
+    hallwayLowerLeftCorner: { x: 73, y: 289, neighbors: ['hallwayMiddleLower', 'hallwayMiddleLeftCorner'] },
+    hallwayMiddleLeftCorner: { x: 73, y: 140, neighbors: ['hallwayLowerLeftCorner','hallwayHigherLeftCorner'] },
+    hallwayHigherLeftCorner: { x: 73, y: 66, neighbors: ['hallwayMiddleLeftCorner','h929'] },
+    hallwayMiddleLower: { x: 178, y: 289, neighbors: ['hallwayLowerLeftCorner', 'hallwayLowerRightCorner','womensBathroom'] },
+    hallwayLowerRightCorner: { x: 305, y: 289, neighbors: ['hallwayMiddleLower'] },
+    h929: { x: 53, y: 66, neighbors: ['hallwayHigherLeftCorner'] },
+    womensBathroom: { x: 228, y: 275, neighbors: ['hallwayMiddleLower'] },
+    Elevator9th: { x: 241, y: 235, neighbors: ['h962'] },
+    h962: { x: 235, y: 205, neighbors: ['Elevator9th'] },
+  },
+};
+
+// JOHN MOLSON BUILDING NODES
+const jmFloorGraphs= {
+  1: {},
+  'S2': {
+    Tunnel: { x: 53, y: 176, neighbors: ['hallway'] },
+    hallway: { x: 168, y: 166, neighbors: ['Tunnel','s2101'] },
+    s2101: { x: 168, y: 60, neighbors: ['hallway'] },
+  },
+};
 
 const BuildingDetails = ({ route }) => {
   const [activeTab, setActiveTab] = useState("Departments");
@@ -210,7 +307,9 @@ const BuildingDetails = ({ route }) => {
       <Modal visible={modalVisible} transparent={true} animationType="fade">
         <Pressable
           style={{ flex: 1, backgroundColor: "rgba(0,0,0,0.8)", justifyContent: "center", alignItems: "center" }}
-          onPress={() => setModalVisible(false)}
+          onPress={(e) => {
+            console.log("Image pressed at:", e.nativeEvent.locationX, e.nativeEvent.locationY);
+            setModalVisible(false)}}
         >
           
           {selectedFloorplan && (
