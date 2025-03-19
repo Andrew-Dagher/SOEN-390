07b1f39078a081ed5069b6267ed4f6b4889557e0
Andrew Harissi Dagher <andrew.h.dagher@gmail.com>
2025-03-19 11:54:03 -0400

    fixed issue in busschedule

--- frontend/app/components/BusSchedule.jsx
+++ frontend/app/components/BusSchedule.jsx
@@ -3,7 +3,6 @@
  * @description A React Native component to display the bus schedule for departures between Loyola and SGW.
  */
 
-import PropTypes from "prop-types"; // Import PropTypes
 import React, { useState, useEffect } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import PropTypes from 'prop-types'; // Import PropTypes
